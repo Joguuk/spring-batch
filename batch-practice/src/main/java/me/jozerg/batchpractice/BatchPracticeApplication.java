97fedbc4676fb9a9c2118df54b81e02ccecab985
joguuk <jjozerg@gmail.com>
2021-06-05 23:53:51 +0900

    Quartz practice

--- batch-practice/src/main/java/me/jozerg/batchpractice/BatchPracticeApplication.java
+++ batch-practice/src/main/java/me/jozerg/batchpractice/BatchPracticeApplication.java
@@ -3,7 +3,9 @@ package me.jozerg.batchpractice;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling       // 스케쥴링 활성화
 @SpringBootApplication
 @EnableBatchProcessing  // 배치 기능 활성화
 public class BatchPracticeApplication {
