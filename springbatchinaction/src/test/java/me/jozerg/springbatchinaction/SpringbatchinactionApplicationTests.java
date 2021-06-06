134b814f90513a114a2d3588f3dba943f1c95836
joguuk <jjozerg@gmail.com>
2021-06-06 18:01:44 +0900

    spring batch inaction
    # ref
     - https://jojoldu.tistory.com

---
+++ springbatchinaction/src/test/java/me/jozerg/springbatchinaction/SpringbatchinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jozerg.springbatchinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchinactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
